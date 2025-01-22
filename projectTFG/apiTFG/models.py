6c7d6d3c48b5879ea00d473d10b0b129ce3d1158
Pablo Peris Solano <pabloperissolano@gmail.com>
2025-01-22 18:32:01 +0100

    Archivos de compilacion quitados + email unico
    Falta hacer autenticacion con tokens, he quitado todos los archivos de compilacion y toda la basura que no es necesaria subirla al github

--- projectTFG/apiTFG/models.py
+++ projectTFG/apiTFG/models.py
@@ -5,9 +5,9 @@ from django.db import models
 
 class Usuario(models.Model):
     nombre = models.CharField(max_length=50)
-    email = models.EmailField(max_length=254)
+    email = models.EmailField(max_length=254, unique=True)
     password = models.CharField(max_length=50)
     admin = models.BooleanField()
 
     def __str__(self):
-        return self.nombre
+        return self.email
